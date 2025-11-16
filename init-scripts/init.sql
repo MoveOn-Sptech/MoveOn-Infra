160b9ad1a04f9e7edfced3bb43dc99659e7660a9
HENRY FRANZ <113705213+Henrytos@users.noreply.github.com>
2025-11-16 11:12:51 -0300

    Refactor SQL tables and foreign key constraints

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -11,25 +11,32 @@ CREATE TABLE IF NOT EXISTS usuario (
     dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS logs (
+
+CREATE TABLE logs (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     tipo VARCHAR(50),
     descricao TEXT,
     dataCriacao TIMESTAMP(6)
 );
 
-CREATE TABLE IF NOT EXISTS Rodovia (
+CREATE TABLE IF NOT EXISTS Concessionaria(
+    idConcessionaria INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nomeConcessionaria VARCHAR(45)
+);
+
+CREATE TABLE Rodovia (
     idRodovia INT NOT NULL PRIMARY KEY auto_increment,
     nomeRodovia VARCHAR(45),
     denominacaoRodovia VARCHAR(45),
-    nomeConcessionaria VARCHAR(45),
     municipioRodovia VARCHAR(45),
     regionalDer VARCHAR(45),
-    regAdmMunicipio VARCHAR(45)
+    fkConcessionaria INT NOT NULL, -- Chave estrangeira para Concessonaeia
+    FOREIGN KEY (fkConcessionaria) REFERENCES Concessionaria(idConcessionaria)
+
 );
 
 -- Tabela Acidente
-CREATE TABLE IF NOT EXISTS Acidente (
+CREATE TABLE Acidente (
     idAcidente INT NOT NULL,
     marcoKm DECIMAL(10, 2), -- Ajuste o tamanho e precisão conforme necessário
     dtHoraAcidente DATETIME, -- Armazenando data e hora do acidente
